a1c261434d772bfc0b01238788b058f4795b30fa
kailibcollins <kailibcollins@icloud.com>
2021-06-02 18:44:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const queryClient = new QueryClient()
+
+export default function App() {
+   return (
+   <QueryClientProvider client={queryClient}>
+     <Example/>
+    
+    </QueryClientProvider>
+   )
+ }
+
+ function Example() {
+  const  [user, setUser] = useState('');
+
+
+
+  const { isLoading, error, data , fetchMore} = useQuery('repoData', () =>
+     fetch(`https://api.github.com/users/${user}`).then(res =>
+       res.json()
+     )
+  )
+
+
+  if (isLoading) return 'Loading...'
+ 
+  if (error) return 'An error has occurred: ' + error.message
+ 
+   return (
+     <div>
+     <input id="username" type="text" placeholder="type..."/>
+     <button type="button" id='submit' onClick={() => {
+       var inputVal = document.getElementById('username').value
+       setUser(inputVal)
+       
+       
+      
+       
+     }}> search </button>
+     <div className = 'profile'>
+       <h1>{data.login}</h1>
+       <p><b>id</b>: {data.id}</p>
+       <p><b>followers</b>: {data.followers}</p>
+       <p><b>following</b>: {data.following}</p>
+       <p><b>type:</b> {data.type}</p>
+       <p><b>public repos:</b> {data.public_repos}</p>
+       <p><b>location:</b> {data.location}</p>
+       <p><b>Email:</b> {data.email}</p>
+       <p><b>Bio:</b> {data.bio}</p>
+       <p><b>Last active:</b> {data.updated_at}</p>
+       </div>
+       
+       
+       
+       
     </div>
-  );
-}
-
-export default App;
+    )
+   }
